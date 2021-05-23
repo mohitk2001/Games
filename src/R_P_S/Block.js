bd8c093de4b25df4bf56d1466c765efdfa5553b3
mohit <mohitkumar.cse19@chitkarauniversity.edu.in>
2021-05-24 03:08:13 +0530

    day-4

--- src/R_P_S/Block.js
+++ src/R_P_S/Block.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import "./RPS.css"
-function Block({image,title}) {
+function Block({image,title,ONClick}) {
 
     
     
     return (
         <>
-          <div  id="image_of_rps">
-          <img src={image} alt="image" id="Img_style"/>  
+          <div id="image_of_rps" onClick={()=>ONClick(title)}>
+          <img src={image} alt="image" id="Img_style"/> 
+          <h1 className="text-center">{title}</h1>
           </div>
         </>
         
